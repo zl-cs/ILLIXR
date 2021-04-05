bfdc8d5d75b821b6f395712c7a96770d8f5a1840
Giordano Salvador <73959795+e3m3@users.noreply.com>
2021-04-05 19:12:22 -0400

    - 'install_deps.sh' now shows the help message when run with no arguments - Updated help message in 'install_deps.sh' - New 'deps.sh' file tracking dependency directory configurations - 'Dockerfile' now defaults to 'Release' for cmake installs - 'Dockerfile' now accepts an 'ACTION' argument for Runner - 'Dockerfile' now defaults to Ubuntu 20.04 - Manual or interactive ('install_deps.sh') script runs default to 'RelWithDebInfo' for cmake installs - Removed 'temp_dir' from 'Dockerfile' - Updated 'GTEST_LOC' to generate from 'deps.sh' in 'common/common.mk' - New 'install_prompt' utility in 'install_deps.sh' for clearing old installs - Canonicalized install script structure (fetch -> build -> install) - Updated CUDA installation detection in 'scripts/install_apt_deps.sh' - Added dependency info logging and configuration loading (default to '.deps/') - Added '.deps' to '.gitignore'

--- scripts/install_gtsam.sh
+++ scripts/install_gtsam.sh
@@ -11,30 +11,51 @@
 ## Source the global helper functions
 . scripts/system_utils.sh
 
+## Source the configurations for our dependencies
+. deps.sh
+
 
 ### Package metadata setup ###
 
-branch_tag_name="kimera-gtsam"
+if [ -z "${dep_name}" ]; then
+    dep_name="${dep_name_gtsam}"
+fi
+
+if [ -z "${src_dir}" ]; then
+    src_dir="${parent_dir_gtsam}/${dep_name_gtsam}"
+fi
+
+if [ -z "${dep_ver}" ]; then
+    dep_ver="${dep_ver_gtsam}"
+fi
+
 repo_url="https://github.com/ILLIXR/gtsam.git"
-gtsam_dir="${opt_dir}/gtsam"
-build_dir="${gtsam_dir}/build"
+build_dir="${src_dir}/build"
 
 case "${build_type}" in
-    Release)          so_file="libgtsam.so" ;;
-    RelWithDebInfo)   so_file="libgtsamRelWithDebInfo.so" ;;
-    Debug)            so_file="libgtsamDebug.so" ;;
-    *)                print_warning "Bad cmake build type '${build_type}'" && exit 1 ;;
+    Release)        so_file="libgtsam.so"
+                    so_file_unstable="libgtsam_unstable.so"
+                    ;;
+    RelWithDebInfo) so_file="libgtsamRelWithDebInfo.so"
+                    so_file_unstable="libgtsam_unstableRelWithDebInfo.so"
+                    ;;
+    Debug)          so_file="libgtsamDebug.so"
+                    so_file_unstable="libgtsam_unstableDebug.so"
+                    ;;
+    *)              print_warning "Bad cmake build type '${build_type}'"
+                    exit 1
+                    ;;
 esac
 
 
 ### Fetch, build and install ###
 
 ## Fetch
-git clone --branch "${branch_tag_name}" "${repo_url}" "${gtsam_dir}"
+git clone --depth 1 --branch "${dep_ver}" "${repo_url}" "${src_dir}"
 
 ## Build
 cmake \
-    -S "${gtsam_dir}" \
+    -S "${src_dir}" \
     -B "${build_dir}" \
     -D CMAKE_BUILD_TYPE="${build_type}" \
     -D CMAKE_INSTALL_PREFIX="${prefix_dir}" \
@@ -48,13 +69,28 @@ make -C "${build_dir}" -j "${illixr_nproc}"
 ## Install
 # Fix suffixed symlinks for the generated shared libaries
 if [ "${build_type}" != "Release" ]; then
+    so_file_release="libgtsam.so"
+    so_file_unstable_release="libgtsam_unstable.so"
+
     cd "${build_dir}/gtsam"
+
     if  [ -f "${so_file}" ]; then
-        if [ -f libgtsam.so ]; then
-            sudo rm -f libgtsam.so
+        if [ -f "${so_file_release}" ]; then
+            sudo rm -f --preserve-root=all "${so_file_release}"
         fi
-        sudo ln -s "${so_file}" libgtsam.so
+        sudo ln -s "${so_file}" "${so_file_release}"
     fi
+
+    if  [ -f "${so_file_unstable}" ]; then
+        if [ -f "${so_file_unstable_release}" ]; then
+            sudo rm -f --preserve-root=all "${so_file_unstable_release}"
+        fi
+        sudo ln -s "${so_file_unstable}" "${so_file_unstable_release}"
+    fi
+
     cd -
 fi
 sudo make -C "${build_dir}" -j "${illixr_nproc}" install
+
+## Log
+log_dependency "${dep_name}" "${deps_log_dir}" "${src_dir}" "${dep_ver}"
