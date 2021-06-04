91db8edb0488ca6d5fcdb3b21ac5878c9698e98d
yihan pang <yihanp2@illinois.edu>
2021-06-03 19:54:33 -0500

    adhere to fastfusion dataset input format

--- offline_TUM/data_loading.hpp
+++ offline_TUM/data_loading.hpp
@@ -54,48 +54,23 @@ load_data() {
 
 	std::map<ullong, sensor_types> data;
 
-	const std::string imu0_subpath = "/groundtruth.txt";
-   	std::ifstream imu0_file {illixr_data + imu0_subpath};
-	if (!imu0_file.good()) {
-		std::cerr << "${ILLIXR_DATA}" << imu0_subpath << " (" << illixr_data << imu0_subpath << ") is not a good path" << std::endl;
-		abort();
-	}
     //for now cam0 is depth
-	const std::string cam0_subpath = "/associate_depth.csv";
-	std::ifstream cam0_file {illixr_data + cam0_subpath};
-	if (!cam0_file.good()) {
-		std::cerr << "${ILLIXR_DATA}" << cam0_subpath << " (" << illixr_data << cam0_subpath << ") is not a good path" << std::endl;
+	const std::string dataset_subpath = "/associate_depth.csv";
+	std::ifstream dataset_file {illixr_data + dataset_subpath};
+	if (!dataset_file.good()) {
+		std::cerr << "${ILLIXR_DATA}" << dataset_subpath << " (" << illixr_data << dataset_subpath << ") is not a good path" << std::endl;
 		abort();
 	}
 	std::string unit_conv;
 	//not skipping the first line 
-	for(CSVIterator row{cam0_file, 0}; row != CSVIterator{}; ++row) {
+	for(CSVIterator row{dataset_file, 0}; row != CSVIterator{}; ++row) {
 	    unit_conv = row[0].c_str();
         auto decimal_start = unit_conv.find(".");
         if(decimal_start != std::string::npos) {unit_conv.erase(decimal_start,1);}
         ullong t = std::stoull(unit_conv);
 //        std::cout<<"depth t: "<<t<<std::endl;
 		data[t].cam0 = {illixr_data +  "/" + row[9]};
-	}
-	//for now cam1 is color
-//	const std::string cam1_subpath = "/depth_rgb.csv";
-	const std::string cam1_subpath = "/associate_color.csv";
-	std::ifstream cam1_file {illixr_data + cam1_subpath};
-	if (!cam1_file.good()) {
-		std::cerr << "${ILLIXR_DATA}" << cam1_subpath << " (" << illixr_data << cam1_subpath << ") is not a good path" << std::endl;
-		abort();
-	}
-	//not skipping the first line and we will skip rgb if no match depth found
-	for(CSVIterator row{cam1_file, 0}; row != CSVIterator{}; ++row) {
-	    unit_conv = row[0].c_str();
-        auto decimal_start = unit_conv.find(".");
-        if(decimal_start != std::string::npos) {unit_conv.erase(decimal_start,1);}
-        ullong t = std::stoull(unit_conv);
-//        std::cout<<"color t: "<<t<<std::endl;
-        if(data.count(t))
-        {
-    		data[t].cam1 = {illixr_data + "/" + row[3]};
-        }
+		data[t].cam0 = {illixr_data +  "/" + row[11]};
 	}
 	std::cout<<"finished data loading"<<std::endl;
 	return data;
