bf6381ea373db3869f1d81be7407730870226144
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-01-01 21:10:25 -0600

    Wrire Nix Flake for timewarp_gl

--- flake.nix
+++ flake.nix
@@ -29,6 +29,38 @@
         glfw
       ];
     };
+
+    packages.x86_64-linux.illixr-timewarp_gl =
+    with import nixpkgs { system = "x86_64-linux"; };
+    clangStdenv.mkDerivation {
+      pname = "illixr-timewarp_gl";
+      version = "2.2.1-latest";
+      src = self;
+      configurePhase = ''
+        export NIX_FLAKES=ON
+      '';
+      buildPhase = ''
+        mkdir -p $out/lib
+        mkdir -p $out/bin
+        mkdir -p $out/obj
+        make -C $src/timewarp_gl plugin.dbg.so
+      '';
+      installPhase = ''
+        # So far the installation is handled by 'Makefile's,
+        # but please keep this 'installPhase' and comments,
+        # or please disable 'installPhase'.
+      '';
+      buildInputs = [
+        libGL
+        glew
+        boost
+        opencv3
+        eigen
+        glfw
+        x11
+      ];
+    };
+
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.illixr-runtime;
   };
 }
