44e2cf82e65b1fc06b7d8908885aa6833280ff1a
yihan pang <yihanp2@illinois.edu>
2022-07-18 17:29:39 -0500

    add debugging prints

--- offload_vio/server_rx/plugin.cpp
+++ offload_vio/server_rx/plugin.cpp
@@ -1,6 +1,9 @@
 #include <filesystem>
 #include <fstream>
 
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
 #include "common/threadloop.hpp"
 #include "common/switchboard.hpp"
 #include "common/data_format.hpp"
@@ -33,6 +36,18 @@ public:
 				std::cerr << "Failed to create data directory.";
 			}
 		}
+
+		if (!filesystem::exists(cam0_dir)) {
+			if (!std::filesystem::create_directory(cam0_dir)) {
+				std::cerr << "Failed to create data directory.";
+			}
+		}
+
+		if (!filesystem::exists(cam1_dir)) {
+			if (!std::filesystem::create_directory(cam1_dir)) {
+				std::cerr << "Failed to create data directory.";
+			}
+		}
 		
 		receive_time.open(data_path + "/receive_time.csv");
 		hashed_data.open(data_path + "/hash_server_rx.txt");
@@ -54,9 +69,12 @@ public:
 		} else {
 			auto now = timestamp();
 			string delimitter = "END!";
+			// auto before_read = timestamp();
 			string recv_data = read_socket->read(); /* Blocking operation, wait for the data to come */
+			// cout << "Block time = " << timestamp() - before_read << endl;
 			buffer_str = buffer_str + recv_data;
 			if (recv_data.size() > 0) {
+				// cout << "Inside if-statement\n";
 				string::size_type end_position = buffer_str.find(delimitter);
 				while (end_position != string::npos) {
 					string before = buffer_str.substr(0, end_position);
@@ -72,11 +90,13 @@ public:
 						hash<std::string> hasher;
 						auto hash_result = hasher(before);
 						hashed_data << vio_input.frame_id() << "\t" << hash_result << endl;
+						// cout << "vio_input: " << &(vio_input) << "\n";
 						ReceiveVioInput(vio_input);
 					}
 					end_position = buffer_str.find(delimitter);
+					// cout << "buffer_str: " << &buffer_str << "\n";
 				}
-				cout << "Recv time = " << timestamp() - now << endl;
+				// cout << "Recv time = " << timestamp() - now << endl;
 			}
 		}
 	}
@@ -103,16 +123,26 @@ private:
 			if (curr_data.rows() != -1 && curr_data.cols() != -1) {
 
 				// Must do a deep copy of the received data (in the form of a string of bytes)
+				// It turns out that the img0_copy, img0, and cam0 all point to the same data.
+				// This results a data race in the socket-based implementation, but not manifest in the ecal implementation.
 				auto img0_copy = std::make_shared<std::string>(std::string(curr_data.img0_data()));
 				auto img1_copy = std::make_shared<std::string>(std::string(curr_data.img1_data()));
+				// cout << "curr_data.img0_data(): " << &(curr_data.img0_data()) << "\n";
+				cout << "img0_copy " << (void*)img0_copy->data() << "\n";
 
-				cv::Mat img0(curr_data.rows(), curr_data.cols(), CV_8UC1, img0_copy->data());
-				cv::Mat img1(curr_data.rows(), curr_data.cols(), CV_8UC1, img1_copy->data());
+				cv::Mat img0(curr_data.rows(), curr_data.cols(), CV_8UC1, (void*)img0_copy->data());
+				cv::Mat img1(curr_data.rows(), curr_data.cols(), CV_8UC1, (void*)img1_copy->data());
+				cout << "img0 " << (void*)(img0.data) << "\n";
 
-				cam0 = std::make_optional<cv::Mat>(img0);
-				cam1 = std::make_optional<cv::Mat>(img1);
+				cam0 = std::make_optional<cv::Mat>(img0.clone());
+				cam1 = std::make_optional<cv::Mat>(img1.clone());
+				cout << "cam0 " << (void*)(cam0.value().data) << "\n";
+
+				cv::imwrite(cam0_dir + std::to_string(vio_input.frame_id()) + ".png", cam0.value());
+				cv::imwrite(cam1_dir + std::to_string(vio_input.frame_id()) + ".png", cam1.value());
+
 			}
-
+// time_point before_put = _m_clock->now();
 			_m_imu_cam.put(_m_imu_cam.allocate<imu_cam_type_prof>(
 				imu_cam_type_prof {
 					_m_clock->now(),
@@ -127,11 +157,13 @@ private:
 					cam1
 				}
 			));	
-			imu_recv << vio_input.real_timestamp() << "," << curr_data.angular_vel().x() << "," << curr_data.angular_vel().y() << "," << curr_data.angular_vel().z() << "," 
+// std::cout << "put takes " << (_m_clock->now() - before_put).count() << " ns \n";
+			imu_recv << curr_data.timestamp() << "," << curr_data.angular_vel().x() << "," << curr_data.angular_vel().y() << "," << curr_data.angular_vel().z() << "," 
 					 << curr_data.linear_accel().x() << "," << curr_data.linear_accel().y() << "," << curr_data.linear_accel().z() << std::endl;
 			
 
 		}
+		// std::this_thread::sleep_for(45ms);
 
 		// unsigned long long after_time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 		// double sec_to_push = (after_time - curr_time) / 1e9;
@@ -151,6 +183,9 @@ private:
 	std::ofstream receive_time;
 	std::ofstream hashed_data;
 	std::ofstream imu_recv;
+
+	std::string cam0_dir = data_path + "/cam0/";
+	std::string cam1_dir = data_path + "/cam1/";
 };
 
 PLUGIN_MAIN(server_reader)
