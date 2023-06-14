293ddff775391951e17f767400d010c31ce05aee
yihan pang <yihanp2@illinois.edu>
2023-06-14 09:42:31 -0500

    trimmed some unnecessary server rx code and updated config for testing

--- offload_sr/server_rx/plugin.cpp
+++ offload_sr/server_rx/plugin.cpp
@@ -20,14 +20,6 @@
 using namespace ILLIXR;
 
 class server_reader : public threadloop {
-private:
-    //std::unique_ptr<video_decoder> decoder;
-    //boost::lockfree::spsc_queue<uint64_t> queue {1000};
-    //std::mutex mutex;
-    //std::condition_variable cv;
-    //cv::Mat img0;
-    //cv::Mat img1;
-    //bool img_ready = false;
 public:
 	server_reader(std::string name_, phonebook* pb_)
 		: threadloop{name_, pb_}
@@ -43,10 +35,6 @@ public:
 				std::cerr << "Failed to create data directory.";
 			}
 		}
-		
-		//receive_time.open(data_path + "/receive_time.csv");
-		// hashed_data.open(data_path + "/hash_server_rx.txt");
-		//dec_latency.open(data_path + "/dec.csv");
 		socket.set_reuseaddr();
 		socket.bind(server_addr);
         frame_count=0;
@@ -153,9 +141,11 @@ private:
         
         cv::Mat img_depth(sr_input.depth_img_data().rows(), sr_input.depth_img_data().columns(), CV_16UC1, depth_copy.data());
 		cv::Mat img_rgb(sr_input.rgb_img_data().rows(), sr_input.rgb_img_data().columns(), CV_8UC3, rgb_copy.data());
+        
         std::cout<<"depth total:  "<< img_depth.total()<<" depth element size: "<<img_depth.elemSize()<<std::endl;
         std::cout<<"rgb total:  "<< img_rgb.total()<<" rgb element size: "<<img_rgb.elemSize()<<std::endl;
-		//cv::Mat img_rgb(sr_input.rgb_img_data().rows(), sr_input.rgb_img_data().columns(), CV_8UC4, rgb_copy.data());
+		
+        //cv::Mat img_rgb(sr_input.rgb_img_data().rows(), sr_input.rgb_img_data().columns(), CV_8UC4, rgb_copy.data());
         
         std::string depth_img = "dump_images/"+std::to_string(sr_input.id()) + "_depth.pgm";
         std::string color_img = "dump_images/"+std::to_string(sr_input.id()) + "_color.png";
@@ -167,6 +157,7 @@ private:
         
         cv::Mat test_depth = img_depth.clone();
         cv::Mat test_rgb = converted_mat.clone(); 
+        
         std::cout << "Address of test_depth: " << &test_depth <<" test rgb: "<< &test_rgb<< std::endl;
         std::cout << "test Reference count: " << test_depth.u->refcount << std::endl;
 
@@ -175,8 +166,10 @@ private:
         //cv::imwrite(color_img,converted_mat);
         //_m_scannet.put(_m_scannet.allocate<scene_recon_path>(scene_recon_path{time_point{}, pose, depth_img, color_img}));
         
+        //pyh not publishing
+        //_m_scannet.put(_m_scannet.allocate<scene_recon_type>(scene_recon_type{time_point{}, pose, test_depth, test_rgb,false}));
+        
         //_m_scannet.put(_m_scannet.allocate<scene_recon_type>(scene_recon_type{time_point{}, pose, img_depth.clone(), converted_mat.clone(),false}));
-        _m_scannet.put(_m_scannet.allocate<scene_recon_type>(scene_recon_type{time_point{}, pose, test_depth, test_rgb,false}));
         //_m_scannet.put(_m_scannet.allocate<scene_recon_type>(scene_recon_type{time_point{}, pose, img_depth, converted_mat,false}));
         // unsigned long long after_time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 		// double sec_to_push = (after_time - curr_time) / 1e9;
